30ac3a888ad236d51b1120a94b2d04b42d061baa
josue <flilipejosue3@gmail.com>
2026-01-14 11:41:58 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+var leonardo = window.document.getElementById('leonardo');
+var samantha = window.document.getElementById('samantha');
+var bruna = window.document.getElementById('bruna');
+var setaDireita = window.document.getElementById('setaDireita');
+var setaEsquerda = window.document.getElementById('setaEsquerda');
+function rolarparaDireita(){
+    samantha.style = "display: none";
+    bruna.style = "display: flex";
+    setaDireita.style = "display: none";
+    setaEsquerda.style = "display: flex; margin-top: 55px";
+
+
+}
+function rolarparaEsquerda(){
+    samantha.style = "display: flex";
+    bruna.style = "display: none";
+    setaEsquerda.style = "display: none";
+    setaDireita.style = "display: flex; margin-top: 55px";
+
+}
